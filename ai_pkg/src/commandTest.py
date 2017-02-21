eddc4445dd8cba6fe1b1cceb79fa2fd54226b7f3
jomjomekid <ekarat.s@ku.th>
2017-02-21 17:48:35 +0700

    fix commandTest.py

--- ai_pkg/src/commandTest.py
+++ ai_pkg/src/commandTest.py
@@ -5,11 +5,11 @@ import rospy
 from std_msgs.msg import String
 
 if __name__ == '__main__':
-    pub = rospy.Publisher('dir', string, queue_size=10)
+    n = 0
+    pub = rospy.Publisher('dir', String, queue_size=10)
     rospy.init_node('clientTest', anonymous=True)
     rate = rospy.Rate(10)
     while not rospy.is_shutdown():
-        int n = 0;
 
         if (n % 4 == 0):
             dir = "R U"
@@ -20,7 +20,7 @@ if __name__ == '__main__':
         elif (n % 4 == 3):
             dir = "L D"
 
-        n++;
+        n += 1
 
         rospy.loginfo(dir)
         pub.publish(dir)
