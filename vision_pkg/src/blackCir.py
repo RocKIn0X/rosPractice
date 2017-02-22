d0f96afaf4ac979afc83d72c043f3bc9b3b1e674
celloAlbatross <buddhinai.k@ku.th>
2017-02-22 22:08:36 +0700

    redCirPosition msg

--- vision_pkg/src/blackCir.py
+++ vision_pkg/src/blackCir.py
@@ -1,35 +1,65 @@
 import cv2
 import numpy as np
+from srv_msg_practice_pkg.srv import vision_service
+from srv_msg_practice_pkg.msg import vision_position
 # import cv2.cv as cv
 
-lower_black = np.array([0,0,0])
-upper_black = np.array([255,150,60])
+lower_red = np.array([0,100,150])
+upper_red = np.array([15,255,255])
+lower_red1 = np.array([165,100,150])
+upper_red1 = np.array([180,255,255])
 cap = cv2.VideoCapture(0)
 
-while(True):
-    ret, frame = cap.read()
+def callback(req):
+    res = vision_position()
+    while(True):
+        ret, frame = cap.read()
 
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    black = cv2.inRange(hsv, lower_black, upper_black)
-    # cv2.imshow('img_show', black)
-    # gray = cv2.cvtColor(black, cv2.COLOR_HSV2GRAY)
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        red = cv2.inRange(hsv, lower_red, upper_red)
+        red1 = cv2.inRange(hsv, lower_red1, upper_red1)
+        realRed = red1
+        # cv2.imshow('img_show', black)
+        # gray = cv2.cvtColor(black, cv2.COLOR_HSV2GRAY)
+        
+        rett, thresh = cv2.threshold(realRed, 127, 255, cv2.THRESH_BINARY)
 
-    circles = cv2.HoughCircles(black,cv2.HOUGH_GRADIENT,1,20,
-                            param1=50,param2=30,minRadius=0,maxRadius=50)
-    # circles = np.unint(np.around(circles))
-    if not(circles == None):
-        for i in circles[0,:]:
-            # draw the outer circle
-            cv2.circle(frame,(i[0],i[1]),i[2],(0,255,0),2)
-            # draw the center of the circle
-            cv2.circle(frame,(i[0],i[1]),2,(0,0,255),3)
-
-    cv2.imshow('img_show', frame)
-    # cv2.imshow('img_show', black)
+        circles = cv2.HoughCircles(realRed,cv2.HOUGH_GRADIENT,4,60,
+                                param1=100,param2=150,minRadius=90,maxRadius=120)
 
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        print(circles)
+        # circles = np.unint(np.around(circles))
+        if not(circles == None):
+            res.x = circles[0]
+            res.y = circles[1]
+            cap.release()
+            cv2.destroyAllWindow()
+            return res 
+           
+            # for i in circles[0,:]:
+            #     # draw the outer circle
+            #     cv2.circle(frame,(i[0],i[1]),i[2],(0,255,0),2)
+            #     # draw the center of the circle
+            #     cv2.circle(frame,(i[0],i[1]),2,(0,0,255),3)
 
-cap.release()
-cv2.destroyAllWindow()
+        
+        # cv2.imshow('red_img', realRed)
+        # cv2.imshow('img_show', frame)
+        # cv2.imshow('tresh', thresh)
+        # cv2.imshow('img_show', black)
+
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindow()
+
+if __name__ == '__main__':
+    rospy.init_node('node_service', anonymous=True)
+    try:
+        service = rospy.Service('name_service', vision_service, callback)
+        rospy.spin()
+    except rospy.ServiceException, e:
+        print "Service call failed: %s" %e
